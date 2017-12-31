f004c6ef644bba2c3969f579cfdac55a2c3420b0
FujiHaruka <fujiharuka@realglobe.jp>
2017-12-31 17:03:14 +0900

    Add mocha test

---
+++ test/domainTest.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+const {
+  findVerse,
+  findHebrewDefinitions
+} = require('../lib/domain')
+
+describe('domain.js', function () {
+  this.timeout(5000)
+
+  it('findVerse', async () => {
+    const osisId = '2sam.2.4'
+    const verse = await findVerse(osisId)
+    assert.ok(verse.length > 0)
+  })
+
+  it('findHebrewDefinitions', async () => {
+    {
+      const wordIds = ['H7225', 'H1254', 'H430', 'H853', 'H8064', 'H853', 'H776']
+      const defs = await findHebrewDefinitions(wordIds)
+      assert.ok(defs['H1254'])
+    }
+    {
+      const strongNums = [7225, 1254, 430, 853, 8064, 853, 776]
+      const defs = await findHebrewDefinitions(strongNums)
+      assert.ok(defs['H1254'])
+    }
+  })
+})
+
+/* global describe it */
